97c7f1629075b08a4b8b8d3c710026e76904037e
Bryan Cabrera <bryancabreram3@gmail.com>
2023-05-18 18:56:41 -0400

    Primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ButtonTimer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from "vue"
+import { BootstrapVue } from "bootstrap-vue"
+import ButtonTimer from "./components/ButtonTimer"
+
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+Vue.use(BootstrapVue)
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ButtonTimer
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
