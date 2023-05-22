a08f8971fe43efa01397f38a6b62c2bac36377c1
Bryan Cabrera <bryancabreram3@gmail.com>
2023-05-21 21:33:43 -0400

    Cambios en main.js

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <template>
   <div id="app">
-    <ButtonTimer />
+    <b-container>
+      <b-alert show variant="danger" dismissible>Alerta</b-alert>
+    </b-container>
   </div>
 </template>
 
 <script>
-import Vue from "vue"
-import { BootstrapVue } from "bootstrap-vue"
-import ButtonTimer from "./components/ButtonTimer"
-
-import "bootstrap/dist/css/bootstrap.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
-
-Vue.use(BootstrapVue)
-
 export default {
-  components: {
-    ButtonTimer
-  }
+  
 }
 </script>
