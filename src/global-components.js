6cdd3bd068ceeceb370c82eb62fac59285d44a91
Bryan Cabrera <bryancabreram3@gmail.com>
2023-05-24 18:06:30 -0400

    update: template

--- src/global-components.js
+++ src/global-components.js
@@ -2,10 +2,10 @@
 import Vue from 'vue'
 import CardRender from '@/views/components/card-render/CardRender.vue'
 import TableRender from '@/views/components/table-render/TableRender.vue'
-import BarraLateral from "./views/components/SidebarPlugin/SideBar.vue";
-import SidebarLink from "./views/components/SidebarPlugin/SidebarLink.vue";
+import SideBar from "@/views/components/SidebarPlugin/SideBar.vue"
+import SidebarLink from "@/views/components/SidebarPlugin/SidebarLink.vue"
 
 Vue.component('card-render', CardRender)
 Vue.component('table-render', TableRender)
-Vue.component('SidebarComponent', BarraLateral) // Cambio de nombre a SidebarComponent
-Vue.component('SidebarLink', SidebarLink)
+Vue.component('side-bar', SideBar)
+Vue.component('sidebar-link', SidebarLink)
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <side-bar>
+      <sidebar-link>
+        <p>Dashboard</p>
+      </sidebar-link>
+    </side-bar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
