bf295e34a1bf4ed63dd043aeb1480858bbcfb3e8
Cristopher-Diaz <cristopherdiaz.c@hotmail.com>
2023-05-28 17:23:57 -0400

    add: test api marvel (heroes)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,34 @@
 <template>
   <b-container>
     <h1>Hola</h1>
+    <!-- Disculpe la roteria... es solo un test -->
+    <div v-for="heroe in heroesResponse" :key="heroe.id">
+      <h3>{{ heroe.name }}</h3>
+      <img :src="heroe.thumbnail.path+'.'+heroe.thumbnail.extension">
+    </div>
   </b-container>
 </template>
 
 <script>
+import HeroesService from './modules/heroes/heroes.service'
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  data () {
+    return {
+      heroesResponse: null,
+      heroesService: new HeroesService(this)
+    }
+  },
+  methods: {
+      
+  },
+  mounted () {
+    console.log(this.heroesService)
+    this.heroesService.callService('getHeroes', null)
+      .then(resp => {
+        console.log(resp.data.data.results)
+        this.heroesResponse = resp.data.data.results
+      })
+  }
 }
 </script>
