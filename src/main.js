41603657921d8d77502724031691e4cd224da1e1
Bryan Cabrera <bryancabreram3@gmail.com>
2023-05-31 19:13:08 -0400

    build: install bootstrap icons

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import BootstrapVue from 'bootstrap-vue'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 import './assets/_custom.scss'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
