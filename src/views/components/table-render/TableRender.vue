3ac581cd9021c58c69a2bf9740818aba3d250805
Bryan Cabrera <bryancabreram3@gmail.com>
2023-05-31 18:29:28 -0400

    style: component tableRender

--- src/views/components/table-render/TableRender.vue
+++ src/views/components/table-render/TableRender.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
     <h1>Table Render</h1>
-    <b-table sticky-header responsive bordered hover head-variant="dark" :items="items"></b-table>
+    <b-table-simple sticky-header responsive bordered head-variant="dark">
+      <b-thead head-variant="dark">
+        <b-tr>
+          <b-th>Edad</b-th>
+          <b-th>Primer nombre</b-th>
+          <b-th>Segundo nombre</b-th>
+          <b-th>Estado</b-th>
+        </b-tr>
+      </b-thead>
+      <b-tbody>
+        <b-tr v-for="(item, index) in items" :key="index">
+          <b-td>{{item.age}}</b-td>
+          <b-td>{{item.first_name}}</b-td>
+          <b-td>{{item.last_name}}</b-td>
+          <b-td><b-badge pill variant="warning">{{item.state}}</b-badge></b-td>
+        </b-tr>
+      </b-tbody>
+    </b-table-simple>
   </div>
 </template>
 
@@ -10,10 +27,10 @@ export default {
     data() {
       return {
         items: [
-          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald', state: true },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw', state: false },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson', state: true },
+          { age: 38, first_name: 'Jami', last_name: 'Carney', state: false }
         ]
       }
     }
