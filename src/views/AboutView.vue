d8e499f18021230268f4075f87e0a7df77a1e2d9
Bryan Cabrera <bryancabreram3@gmail.com>
2023-06-28 17:31:14 -0400

    perf: Component table-render, AboutView and characters service

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,28 +1,50 @@
 <template>
   <b-container>
-    <table-render :rows="items" :fields="fields"></table-render>
+    <table-render :rows="marvelComics" :fields="fields"></table-render>
     <card-render></card-render>
   </b-container>
 </template>
 
 <script>
+import CharactersService from "./modules/characters/characters.service"
+
 export default {
   name: 'AboutView',
   data() {
     return {
-      // Arreglo a iterar en la tabla
+      // Array con los nombres de los campos en la cabecera de la tabla
+      // key: Información del array a iterar en la tabla
+      // label: Nombre de la cabecera a mostrar en la tabla
       fields: [
-        'first_name',
-        'last_name',
-        'age',
-        {key: 'actions'}
-        ],
-      items:  [
-        { age: 40, last_name: 'Dickerson', first_name: 'Macdonald' },
-        { age: 21, last_name: 'Larsen', first_name: 'Shaw' },
-        { age: 89, last_name: 'Geneva', first_name: 'Wilson' },
-        { age: 38, last_name: 'Jami', first_name: 'Carney' }
-      ]
+        { key: 'id', label: 'ID' },
+        { key: 'title', label: 'Nombre del comic' },
+        { key: 'pageCount', label: 'Total de páginas' },
+        { key: 'modified', label: 'Ultimos cambios' },
+        { key: 'actions', label: 'Acciones' }
+      ],
+      // Instancia del service que hace el llamado a la API
+      comicsService: new CharactersService(this),
+      // Array con info para mostrar el contenido de la tabla 
+      marvelComics: []
+    }
+  },
+  // Al momento de cargar la vista se llama a la API y se crea un objeto con la info justa para mandarla al array de marvelComics
+  mounted() {
+    this.comicsService.callService('getComics')
+      .then(res => res.data.data.results.forEach(data => {
+        const dataObj = {
+          id: data.id,
+          title: data.title,
+          pageCount: data.pageCount,
+          modified: this.dateFormat(data.modified)
+        }
+        this.marvelComics.push(dataObj)
+      }))
+  },
+  // Formateo de fecha
+  methods: {
+    dateFormat(date) {
+      return new Date(date).toLocaleDateString('en-GB')
     }
   }
 }
--- src/views/components/table-render/TableRender.vue
+++ src/views/components/table-render/TableRender.vue
@@ -1,14 +1,12 @@
 <template>
-  <div>
-    <h1 class="my-4">Table Render</h1>
+  <div class="mt-4">
     <div class="border rounded p-4">
       <div class="d-flex justify-content-between align-items-center mb-4">
         <p class="m-0">Comics correspondientes a Marvel</p>
         <b-button variant="info">Ver más</b-button>
       </div>
       <div>
-        <!-- <b-badge pill variant="primary">{{row.state}}</b-badge> -->
-        <b-table :items="rows" :fields="fields">
+        <b-table :items="rows" :fields="fields" responsive fixed>
           <template #head(actions)="scope">
             <div class="text-center">{{ scope.label }}</div>
           </template>
@@ -16,6 +14,16 @@
             <div class="text-center">
               <b-button variant="success"><b-icon icon="arrow-up"></b-icon></b-button>
               <b-button variant="danger" class="ml-2"><b-icon icon="exclamation-triangle"></b-icon></b-button>
+            </div>
+          </template>
+          <template #head(calification)="scope">
+            <div class="text-center">{{ scope.label }}</div>
+          </template>
+          <template #cell(calification)="scope">
+            <div class="text-center">
+              <b-badge pill variant="primary">
+                {{ scope.item.calification }}
+              </b-badge>
             </div>
           </template>
         </b-table>
@@ -32,8 +40,7 @@ export default {
       required: true
     },
     fields: {
-      type: Array,
-      required: true
+      type: Array
     }
   }
 };
