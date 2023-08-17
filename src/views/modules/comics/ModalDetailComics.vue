1e5d1424f557c3db50251711bb6d4c22b6e957f7
Jozz16 <jo633554@gmail.com>
2023-08-16 22:03:01 -0400

    add: modal detail comics

---
+++ src/views/modules/comics/ModalDetailComics.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-modal id="modal-detail" title="BootstrapVue" button-size="sm" hide-footer> 
+    <p class="my-2">This modal has small footer buttons</p>
+    
+    {{ marvelDetail }}
+  </b-modal>
+</template>
+
+<script>
+export default {
+    props: ['marvelDetail']
+};
+</script>
+
+<style>
+</style>
